e472a6598b35a2a7ebb342718ac28f3268a9e8a1
محمد الباقر <147254383+MohmmadALbaqer@users.noreply.github.com>
2023-11-23 23:49:02 +0300

    install.py

---
+++ install.py
@@ -0,0 +1,26 @@
+import os 
+from colorama import Fore, Style
+from termcolor import colored
+import pyfiglet
+import random
+
+colors = ['grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white',
+          'on_grey', 'on_red', 'on_green', 'on_yellow']
+
+selected_color = random.choice(colors)
+
+text = 'INSTALL'
+
+lo = pyfiglet.figlet_format(text)
+colored_lo = colored(lo, color=selected_color)
+
+print(colored_lo)
+
+
+os.system("pip install socket")
+os.system("pip install colorama")
+os.system("pip install termcolor")
+os.system("pip install pyfiglet")
+os.system("pip install Flask")
+os.system("pip install sys")
+os.system("sudo apt-get install g++")
