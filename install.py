a8d99ab9951d43784c28cab5938e7738e7c895a3
محمد الباقر <147254383+MohmmadALbaqer@users.noreply.github.com>
2023-11-24 00:22:10 +0300

    install.py

--- install.py
+++ install.py
@@ -1,15 +1,14 @@
-import os 
+from flask import Flask, render_template
 from colorama import Fore, Style
 from termcolor import colored
 import pyfiglet
 import random
 
-colors = ['grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white',
-          'on_grey', 'on_red', 'on_green', 'on_yellow']
+colors = ['grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
 
 selected_color = random.choice(colors)
 
-text = 'INSTALL'
+text = 'Application'
 
 lo = pyfiglet.figlet_format(text)
 colored_lo = colored(lo, color=selected_color)
@@ -17,10 +16,22 @@ colored_lo = colored(lo, color=selected_color)
 print(colored_lo)
 
 
-os.system("pip install socket")
-os.system("pip install colorama")
-os.system("pip install termcolor")
-os.system("pip install pyfiglet")
-os.system("pip install Flask")
-os.system("pip install sys")
-os.system("sudo apt-get install g++")
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/detect_spy_devices')
+def detect_spy_devices():
+
+    spy_detected = False
+
+    if spy_detected:
+        return "تم اكتشاف جهاز تجسس. يُرجى أخذ الحيطة والحذر."
+    
+    return "لا توجد مشاكل تجسس."
+
+if __name__ == '__main__':
+    app.run(debug=True)
